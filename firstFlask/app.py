721b42906c4e5eff284e2c361bd38bcc0fedfc64
LuizMiguelGaio <devluizmiguelgaio@gmail.com>
2023-11-08 17:23:27 -0300

    Subindo o app com flask pela primeira vez.

---
+++ firstFlask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        return 'Hello'
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    with app.app_context(): #cria contexto da app
+        db.create_all()
+    app.run(debug=True)
+
