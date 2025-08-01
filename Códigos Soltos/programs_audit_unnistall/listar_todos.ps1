f1b041d65594c4949cfa9b58ab059a03664f9ee3
LuizMiguelGaio <devluizmiguelgaio@gmail.com>
2025-08-01 09:33:06 -0300

    add

---
+++ Códigos Soltos/programs_audit_unnistall/listar_todos.ps1
@@ -0,0 +1,7 @@
+﻿Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* ,
+                 HKLM:\Software\Wow6432Node\Microsoft\* |
+Where-Object { $_.DisplayName } |
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Sort-Object DisplayName |
+Out-File "C:\Relatorio_Programas_Instalados.txt"
+Write-Host "Relatorio criado no $Out-file"
